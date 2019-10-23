64908cf821086456bebaa45cc5e20cbccdef013a
Dong Nguyen <nvdkg1999@gmail.com>
2019-10-23 09:54:26 +0700

    Infopane editing

---
+++ src/components/infopane/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import InfoPane from '../infopane'
+export default class InfoPane extends Component{
+    render(){
+        return (
+            <View 
+                width={100}
+                height={100}
+                color={"#000000"}
+            >
+                <Text>Mother fucker the life!</Text>
+            </View>
+
+        )
+    }
+}
--- src/components/screen/test.js
+++ src/components/screen/test.js
@@ -3,6 +3,7 @@ import {View} from "react-native"
 import {AdjacencyMatrixGraph} from "../../tool/graph_theory/graphs"
 import {RecursiveTraverse, DepthFirstSearch} from "../../tool/graph_theory/algorithms"
 import {BreadthFirstSearch} from "../../tool/graph_theory/algorithms"
+import InfoPane from '../infopane'
 
 export default class Test extends Component {
   static navigationOptions = {
@@ -10,20 +11,20 @@ export default class Test extends Component {
   };
 
   render() {
-    let graph = new AdjacencyMatrixGraph(5,7,false);
-    graph.addEdge({u: 1, v: 2});
-    graph.addEdge({u: 1, v: 3});
-    graph.addEdge({u: 1, v: 4});
-    graph.addEdge({u: 1, v: 5});
-    graph.addEdge({u: 2, v: 3});
-    graph.addEdge({u: 2, v: 4});
-    graph.addEdge({u: 4, v: 5});
-    graph.display();
-    let algorithms = new DepthFirstSearch(graph, 1);//start traverse at node 1
-    algorithms.run();
-    console.log(algorithms.getStates());
+    // let graph = new AdjacencyMatrixGraph(5,7,false);
+    // graph.addEdge({u: 1, v: 2});
+    // graph.addEdge({u: 1, v: 3});
+    // graph.addEdge({u: 1, v: 4});
+    // graph.addEdge({u: 1, v: 5});
+    // graph.addEdge({u: 2, v: 3});
+    // graph.addEdge({u: 2, v: 4});
+    // graph.addEdge({u: 4, v: 5});
+    // graph.display();
+    // let algorithms = new DepthFirstSearch(graph, 1);//start traverse at node 1
+    // algorithms.run();
+    // console.log(algorithms.getStates());
     return (
-      <View> </View>
+      <InfoPane/>
     );
   }
 }
