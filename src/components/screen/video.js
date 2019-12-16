c385b79386eae6775d3204aa278d2eae4bd688cf
Dong Nguyen <nvdkg1999@gmail.com>
2019-12-16 15:08:31 +0700

    Fix conflicts screen Tutorial and VideoScreen

--- src/components/screen/video.js
+++ src/components/screen/video.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 
 export default class VideoScreen extends Component{
     static navigationOptions = {
-        title: 'Tutorial'
+        title: 'Videos'
     };
     render(){     
         return(
